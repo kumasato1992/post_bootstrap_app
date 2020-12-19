46ccc183d167503e260b8c28e5bde16ce6d70429
Masato Kumatani <masato@MasatonoMacBook-Pro.local>
2020-12-19 19:19:33 +0900

    postsコントローラとビューファイルを作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def adit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
