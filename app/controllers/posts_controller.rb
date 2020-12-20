9cc631605b5407edaa8b95fffba3f55e63d3c064
Masato Kumatani <masato@MasatonoMacBook-Pro.local>
2020-12-20 20:19:01 +0900

    コントローラでフラッシュメッセージを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,12 +22,12 @@ class PostsController < ApplicationController
 
   def update
     @post.update!(post_params)
-    redirect_to post
+    redirect_to @post, notice: "更新しました"
   end
 
   def destroy
     @post.destroy!
-    redirect_to root_path
+    redirect_to root_path, alert: "削除しました"
   end
 
   private
